180dcd9c87ae1a6f5f44ded11a568deac7dda03a
Sodam Baek <bsodam@gmail.com>
2019-01-17 23:46:32 +0900

    [update] login/out toggle

--- mysite/views.py
+++ mysite/views.py
@@ -2,12 +2,18 @@ from django.views.generic.base import TemplateView
 from django.views.generic import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
 
 from django.apps import apps
 
 
 class HomeView(TemplateView):
     template_name = "index.html"
+
+    def get(self, request, *args, **kwargs):
+        context = {'username': request.user.username}
+        return render(request, 'index.html', context=context)
+
 
 class AboutView(TemplateView):
     template_name = "about.html"
