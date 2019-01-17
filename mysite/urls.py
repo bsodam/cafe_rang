9ee5f6f2b5b72d06de101584f4a7c151111d8a0f
Sodam Baek <bsodam@gmail.com>
2019-01-17 21:33:50 +0900

    [feature] registration and login/logout

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from mysite import views
 
@@ -22,4 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.HomeView.as_view(), name="home"),
     path('about/', views.AboutView.as_view(), name="about"),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.UserCreateView.as_view(), name='signup'),
+    path('registered/', views.RegisterdView.as_view(), name='registered'),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,8 +1,21 @@
 from django.views.generic.base import TemplateView
+from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+
 from django.apps import apps
+
 
 class HomeView(TemplateView):
     template_name = "index.html"
 
 class AboutView(TemplateView):
     template_name = "about.html"
+
+class UserCreateView(CreateView):
+    template_name = 'registration/signup.html'
+    form_class = UserCreationForm
+    success_url = '/registered'
+
+class RegisterdView(TemplateView):
+    template_name = 'registration/registered.html'
