88e2fbc9df0f1eab05d07f4de8f1253af65282a3
Sodam Baek <bsodam@gmail.com>
2019-01-19 00:05:32 +0900

    [feature] cafe name based search

--- cafes/urls.py
+++ cafes/urls.py
@@ -6,4 +6,5 @@ app_name = 'cafes'
 urlpatterns = [
     path('list/', views.ListView.as_view(), name='list'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('results/', views.ResultListView.as_view(), name='results')
 ]
--- cafes/views.py
+++ cafes/views.py
@@ -14,3 +14,13 @@ class ListView(ListView):
 class DetailView(DetailView):
 	model = Cafe
 	# template_name = 'cafes/cafe_detail.html'
+
+class ResultListView(ListView):
+    template_name = 'cafes/results.html'
+    context_object_name = 'cafe_list'
+
+    def get_queryset(self):
+        search_keyword = self.request.GET.get('search_keyword','')
+        print("ResultListView called")
+        print(search_keyword)
+        return Cafe.objects.filter(cafe_name__contains=search_keyword)
