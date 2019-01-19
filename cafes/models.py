436009bf7e5a302c44839de3b4a14aeb8a5bff2e
Sodam Baek <bsodam@gmail.com>
2019-01-19 19:08:06 +0900

    [update] review

--- cafes/models.py
+++ cafes/models.py
@@ -9,6 +9,17 @@ class Cafe(models.Model):
     cafe_menu = models.TextField(blank=True)
     # cafe_image = models.ImageField(blank=True)
 
+    def __str__(self):
+        return self.cafe_name
+
 class CafeKeyword(models.Model):
     cafe_id = models.PositiveIntegerField()
     cafe_keyword = models.CharField(max_length=30)
+
+    # def __str__(self):
+    #     return [self.cafe_id, self.cafe_keyword]
+
+    # fieldsets = [
+    #     ('cafe_id', {'fields': ['cafe_id']}),
+    #     ('cafe_keyword', {'fields': ['cafe_keyword']}),
+    # ]
